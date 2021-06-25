0ba6766ca21caef50027e313f3367eded2f72dc4
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-24 22:22:40 -0300

    Update Button.tsx

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from 'react';
 
+import '../styles/button.scss';
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button(){
-    //let counter = 0;
-    const [counter, setCounter] = useState(0)
-
-    function increment() {
-        setCounter(counter + 1)
-        console.log(counter);
-    }
-
+export function Button(props: ButtonProps) {
     return (
-        <button onClick={increment}>
-            {counter}
-        </button>
+        <button className="button" {...props} />
     )
 }
 
