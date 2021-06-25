96ae5e6701778d4b1dd10cf9f0a454d25a4170dd
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-24 22:22:54 -0300

    Update Home.tsx

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
+import { Button } from '../components/Button';
 
 export function Home() {
     return (
@@ -25,9 +26,9 @@ export function Home() {
                         <input type="text"
                             placeholder="Digite o código da sala"
                         />
-                        <button type="submit">
+                        <Button type="submit">
                             Entra na sala
-                        </button>
+                        </Button>
                     </form>
                 </div>
             </main>
