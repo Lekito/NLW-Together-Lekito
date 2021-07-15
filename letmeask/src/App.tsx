0e5ca06a8a58ac0df40405fda6ef0cbd6a8f7862
Lekito <alexlekito075@gmail.com>
2021-07-15 00:48:59 -0300

    Update App.tsx
    Adicionando as rotas na aplicação!

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,11 +1,17 @@
-//import { Home } from "./pages/Home";
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-
+/* a propriedade exact serva para somente deixar exatamente a pagina unica. Ou seja, 
+ se não tivesse exact dentro do 'Route path="/" ' quando fosse utilizar path abaixo apareciria as duas telas. Outr a forma de utilizar esse parametro é só escrever 'exact' , pois
+ o true já fica implicito. */
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
