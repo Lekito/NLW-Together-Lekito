34605e746d193c1857414cea41b271a0892d3075
Lekito <alexlekito075@gmail.com>
2021-07-16 20:57:00 -0300

    Autenticação
    Autenticação usando contextos do React, async e await. Isso para levar os dados do usuário com conta na Google para aplicação.

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,16 +1,57 @@
+import { createContext, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { auth, firebase } from './services/firebase';
 
 /* a propriedade exact serva para somente deixar exatamente a pagina unica. Ou seja, 
  se não tivesse exact dentro do 'Route path="/" ' quando fosse utilizar path abaixo apareciria as duas telas. Outr a forma de utilizar esse parametro é só escrever 'exact' , pois
  o true já fica implicito. O exact é boleano*/
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
 function App() {
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider)
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user
+
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account.')
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+
+  }
+
   return (
     <BrowserRouter>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </ AuthContext.Provider>
     </BrowserRouter>
   );
 }
